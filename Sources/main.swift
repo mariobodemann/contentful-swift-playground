6082fb7e766f4d33c555e52fae521831bafac8ee
Mario Bodemann <mario@contentful.com>
2017-03-10 15:23:58 +0100

    add code to call contentful

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,22 @@
-print("Hello, world!")
+import Dispatch
+import Contentful
+
+let client = Client(spaceIdentifier: "cfexampleapi", accessToken: "b4c0n73n7fu1")
+
+DispatchQueue.global().sync {
+  let group = DispatchGroup()
+  group.enter()
+  
+  client.fetchEntry(identifier:"nyancat") { (result) in
+    switch result {
+        case let .success(entry):
+            print(entry)
+        case .error(_):
+            print("Error!")
+    }
+    group.leave()
+  }
+
+  group.wait()
+  print("Done")
+}
