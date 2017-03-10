0f92cbdb7443263f79feaf4fca8e61b87480018b
Mario Bodemann <mario@contentful.com>
2017-03-10 14:22:37 +0100

    Add contentful dependency

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "simple-swift-contentful"
+    name: "simple-swift-contentful",
+    dependencies: [
+	.Package(url: "http://github.com/contentful/contentful.swift.git", majorVersion: 0),
+	]
 )
